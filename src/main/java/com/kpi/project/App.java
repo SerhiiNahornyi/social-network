3d1df9c0a3318bc331c388a410cda0ad9fd7b619
Serhii Nahornyi <sergiy3344@gmail.com>
2020-11-08 22:07:55 +0200

    Add compiler settings (#4)
    * Add compiler plugin
    
    * Add checkstyle plugin
    
    * Add git CI
    
    Co-authored-by: Serhii Nahornyi <snahornyi@rubiconproject.com>

--- src/main/java/com/kpi/project/App.java
+++ src/main/java/com/kpi/project/App.java
@@ -3,8 +3,10 @@ package com.kpi.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("checkstyle:hideutilityclassconstructor")
 @SpringBootApplication
 public class App {
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
