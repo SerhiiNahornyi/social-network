85357b27d66b50a1a8e8b576178267ffe7369307
Serhii Nahornyi <snahornyi@rubiconproject.com>
2020-11-13 20:22:32 +0200

    Add MySql DB to project

--- src/main/java/com/kpi/project/service/UserService.java
+++ src/main/java/com/kpi/project/service/UserService.java
@@ -1,6 +1,7 @@
 package com.kpi.project.service;
 
 import com.kpi.project.model.User;
+import com.kpi.project.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,11 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         final User user = new User();
         user.setEmail("user");
         user.setPassword("pass");
-        return user;
+        userRepository.save(user);
+        return userRepository.findByEmail("user");
     }
 }
