8db8919cd082b44dd0687ccaf8ece7eddb8a29c9
Serhii Nahornyi <snahornyi@rubiconproject.com>
2020-11-13 20:27:22 +0200

    Fixes after review

--- src/main/java/com/kpi/project/repository/UserRepository.java
+++ src/main/java/com/kpi/project/repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    User findByEmail(String username);
+    User findByEmail(String email);
 }
