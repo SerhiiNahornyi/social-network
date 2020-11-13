1c1b2b8a4963ce7de923ea227198af680d1b0b57
Oleg Levochkin <oleg.lev0chkin10@gmail.com>
2020-11-13 02:53:06 +0200

    Create user model (#13)
    Create basic user model for authentication

---
+++ src/main/java/com/kpi/project/model/User.java
@@ -0,0 +1,50 @@
+package com.kpi.project.model;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+public class User implements UserDetails {
+
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
