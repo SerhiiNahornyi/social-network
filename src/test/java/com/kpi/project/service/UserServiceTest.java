38d1fb67552c0fa06867e883a2ce044c7030551f
Serhii Nahornyi <sergiy3344@gmail.com>
2020-11-14 14:06:13 +0200

    Make maven to work with jupiter tests (#38)

--- src/test/java/com/kpi/project/service/UserServiceTest.java
+++ src/test/java/com/kpi/project/service/UserServiceTest.java
@@ -1,11 +1,16 @@
 package com.kpi.project.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class UserServiceTest {
 
     @Test
+    @DisplayName("some test")
     public void test(){
-
+        assertThat(true).isEqualTo(true);
     }
 }
