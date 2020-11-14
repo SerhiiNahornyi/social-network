44f0652a3cccaa430c4544192a00ac97cf2334c4
olegl <oleg.lev0chkin10@gmail.com>
2020-11-15 01:52:51 +0200

    Update user role endpoint

--- src/main/java/com/kpi/project/validate/UserValidator.java
+++ src/main/java/com/kpi/project/validate/UserValidator.java
@@ -2,8 +2,8 @@ package com.kpi.project.validate;
 
 import com.kpi.project.model.User;
 import com.kpi.project.model.dto.UserDto;
+import com.kpi.project.model.enums.Role;
 import com.kpi.project.model.exception.ValidatorException;
-import com.kpi.project.model.userRole.Role;
 import com.kpi.project.repository.UserRepository;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
@@ -40,23 +40,23 @@ public class UserValidator {
 
     public void validateUser(UserDto userToValidate) {
         if (!Objects.equals(userToValidate.getPassword(), userToValidate.getMatchingPassword())) {
-            throw new IllegalArgumentException("Passwords does not match");
+            throw new ValidatorException("Passwords does not match");
         }
         if (userToValidate.getPassword().length() < 4) {
-            throw new IllegalArgumentException("Password length must be minimum of 4 symbols");
+            throw new ValidatorException("Password length must be minimum of 4 symbols");
         }
         if (StringUtils.isBlank(userToValidate.getEmail())) {
-            throw new IllegalArgumentException("Email should be present");
+            throw new ValidatorException("Email should be present");
         }
         if (StringUtils.isBlank(userToValidate.getUsername())) {
-            throw new IllegalArgumentException("Username should be present");
+            throw new ValidatorException("Username should be present");
         }
         final User user = userRepository.findByEmailOrUsername(userToValidate.getEmail(), userToValidate.getUsername());
         if (Objects.nonNull(user)) {
             if (Objects.equals(user.getEmail(), userToValidate.getEmail())) {
-                throw new IllegalArgumentException("Email already exists");
+                throw new ValidatorException("Email already exists");
             }
-            throw new IllegalArgumentException("Username already exists");
+            throw new ValidatorException("Username already exists");
         }
     }
 }
