cafe13172de46bc0a96af6f8d5f013020a714790
vKLimenko <vladklimenko65@gmail.com>
2021-04-03 01:01:23 +0300

    Make id and password fields invisible for user

--- src/main/java/com/kpi/project/model/dto/UserDto.java
+++ src/main/java/com/kpi/project/model/dto/UserDto.java
@@ -1,6 +1,5 @@
 package com.kpi.project.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Set;
--- src/main/java/com/kpi/project/model/mapper/UserMapper.java
+++ src/main/java/com/kpi/project/model/mapper/UserMapper.java
@@ -2,23 +2,16 @@ package com.kpi.project.model.mapper;
 
 import com.kpi.project.model.User;
 import com.kpi.project.model.dto.UserDto;
-import com.kpi.project.model.enums.Role;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     User dtoToUser(UserDto userDto);
 
-
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "password", ignore = true)
     UserDto userToDto(User user);
-
 
 }
